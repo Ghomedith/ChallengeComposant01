7cbc96b43673797fc61f3ef048ae2000e4dbb861
Ghomedith <miche.edith@yahoo.fr>
2024-05-28 11:10:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from './block/block.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlockComponent,UserProfileComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'challengeComposant01';
 }
+
